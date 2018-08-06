59c2a5eda465ded23dd023a0dd633a7ac3cbc1b5
brown2206 <brown.angelo.jr@gmail.com>
2018-08-06 01:06:27 -0400

    added contact section

--- js/script.js
+++ js/script.js
@@ -36,6 +36,8 @@ $(document).ready(function() {
 
 
     var skillsTopOffset = $(".skillsSection").offset().top;
+    var statsTopOffset = $(".statsSection").offset().top;
+    var countUpFinished = false;
 
     $(window).scroll(function() {
 
@@ -54,7 +56,19 @@ $(document).ready(function() {
 
         }
 
+        if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+            $(".counter").each(function() {
+                var element = $(this);
+                var endVal = parseInt(element.text());
+
+                element.countup(endVal);
+            })
+
+            countUpFinished = true;
+        }
+
     });
+
 
 
 });
