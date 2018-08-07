edb5aa8022790eae606d87cb4f818cf5f9f420cc
brown2206 <brown.angelo.jr@gmail.com>
2018-08-07 00:05:11 -0400

    add copyright

--- js/script.js
+++ js/script.js
@@ -78,7 +78,26 @@ $(document).ready(function() {
             easing: 'linear',
             queue: false
         }
-    })
+    });
+
+    $("#filters a").click(function() {
+
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        return false;
+    });
 
 
 });
