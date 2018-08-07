426151f383fe0631f5e42b41b702c7d1ba3087f7
brown2206 <brown.angelo.jr@gmail.com>
2018-08-06 23:48:19 -0400

    styling css

--- js/script.js
+++ js/script.js
@@ -69,6 +69,16 @@ $(document).ready(function() {
 
     });
 
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    })
 
 
 });
