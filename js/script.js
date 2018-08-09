4f2e6b4a61cf42f5993bdfa10ca29c195e021a39
brown2206 <brown.angelo.jr@gmail.com>
2018-08-08 20:00:25 -0400

    updates to css navbar

--- js/script.js
+++ js/script.js
@@ -100,4 +100,40 @@ $(document).ready(function() {
     });
 
 
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        var body = $("body");
+
+        if($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px")
+            body.addClass("fixedNav");
+        }
+        else {
+            body.css("padding-top", 0)
+            body.removeClass("fixedNav");
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
